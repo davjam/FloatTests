c2bde883d4b0357742a0228da15cda525bacf9cc
davjam <45334176+davjam@users.noreply.github.com>
2021-10-01 13:44:44 +0100

    reordered asinh (usual case first), updated comments.

--- MyFloatC.c
+++ MyFloatC.c
@@ -15,7 +15,7 @@ The same changes are needed for float, double, long double (anything else) versi
 double asinh(double x)
 {
   /* mathematically:
-     asinh(x) = log (x + sqrt (x * x + 1.0))
+     asinh(z) = log (z + sqrt (z * z + 1.0))
   */
 
   double z;
@@ -31,30 +31,7 @@ double asinh(double x)
 
   const double asinhCutover = pow(2,DBL_MAX_EXP/2); // 1.3407807929943e+154
 
-  if (z >= asinhCutover)
-  /* above this, z*z+1 == z*z, so we can simplify
-     (and avoid z*z being infinity).
-      asinh(z) = log (z + sqrt (z * z + 1.0))
-               = log (z + sqrt (z * z      ))
-               = log (2 * z)
-               = log 2 + log z
-
-      Choosing asinhCutover is a little tricky.
-      We'd like something that's based on the nature of
-      the numeric type (DBL_MAX_EXP, etc).
-      If x = asinhCutover, then we need:
-         (1) x*x == x*x + 1
-         (2) x*x < Infinity
-         (3) log (2*x) = log 2 + log x.
-      For float:
-         9.490626562425156e7 is the smallest value that
-         achieves (1), but it fails (3). (It only just fails,
-         but enough to make the function erroneously non-monotonic).
-         1.3407807929942596e154 is the largest value
-         that achieves (2).
-    */
-    z = __fast_log(2) + __fast_log(z);
-  else
+  if (z < asinhCutover)
   /* Use log1p to avoid cancellation with small x. Put
      x * x in denom, so overflow is harmless. 
      asinh(z) = log   (z + sqrt (z * z + 1.0))
@@ -67,8 +44,31 @@ double asinh(double x)
               = log1p (z + z * z / (sqrt (z * z + 1.0) + 1.0))
      */
     z = __fast_log1p (z + z * z / (__fast_sqrt (z * z + 1.0) + 1.0));
+  else
+  /* above this, z*z+1 == z*z, so we can simplify
+     (and avoid z*z being infinity).
+      asinh(z) = log (z + sqrt (z * z + 1.0))
+               = log (z + sqrt (z * z      ))
+               = log (2 * z)
+               = log 2 + log z
 
-  return copysign(z, x);
+      Choosing asinhCutover is a little tricky.
+      We'd like something that's based on the nature of
+      the numeric type (DBL_MAX_EXP, etc).
+      If c = asinhCutover, then we need:
+         (1) c*c == c*c + 1
+         (2) c*c < Infinity
+         (3) log (2*c) = log 2 + log c.
+      For float:
+         9.490626562425156e7 is the smallest value that
+         achieves (1), but it fails (3). (It only just fails,
+         but enough to make the function erroneously non-monotonic).
+         1.3407807929942596e154 is the largest value
+         that achieves (2).
+    */
+    z = __fast_log(2) + __fast_log(z);
+
+  return copysign(z, x); //ensure 0.0 -> 0.0 and -0.0 -> -0.0.
 }
 
 double atanh(double x)
@@ -93,5 +93,5 @@ double atanh(double x)
            = 0.5 * log1p ((1.0 + x - 1.0 + x) /(1.0 - x)) 
            = 0.5 * log1p ((2.0 * x ) / (1.0 - x))  */
   z = 0.5 * __fast_log1p ((z + z) / (1.0 - z));
-  return copysign(z, x);
+  return copysign(z, x); //ensure 0.0 -> 0.0 and -0.0 -> -0.0.
 }
